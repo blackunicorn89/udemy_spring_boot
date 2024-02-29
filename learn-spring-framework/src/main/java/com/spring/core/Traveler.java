c5854d4e5a7b985234565549d0edd2ce85bbcac9
Antti Helander <antti.helander@hubble.fi>
2024-02-29 15:22:04 +0200

    First push

---
+++ learn-spring-framework/src/main/java/com/spring/core/Traveler.java
@@ -0,0 +1,12 @@
+package com.spring.core;
+
+public class Traveler {
+    Car car = null;
+    public Traveler() {
+        this.car = new Car();
+    }
+
+    public void startJourney() {
+        this.car.move();
+    }
+}
