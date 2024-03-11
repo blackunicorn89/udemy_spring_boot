b15bcf9d5e69382bd0b49e94076528250f85973e
Antti Helander <antti.helander@hubble.fi>
2024-03-11 16:40:57 +0200

    Went through creating custom Query methods (section 22)

--- spring-data-jpa-course/src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
+++ spring-data-jpa-course/src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
@@ -3,6 +3,10 @@ package com.springdatajpa.springboot.repository;
 import com.springdatajpa.springboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -13,5 +17,45 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     /* Returns an Optional which contains the found product entry by using its id as search criteria. If no product entry is found, this
   method an empty Optional */
     Optional<Product> findById(Long id);
+
+    /*Returns the found list of product entries whose name or description is given
+    * as a method parameter. If no product entries is found, this method returns an empty list*/
+    List<Product>findByNameOrDescription(String name, String description);
+
+  /*Returns the found list of product entries whose name and description is given
+   * as a method parameter. If no product entries is found, this method returns an empty list*/
+
+ List<Product>findByNameAndDescription(String name, String description);
+
+ /*Return the distinct product whose name is given as a method parameter.
+ * If no product empty is found, this method returns null. */
+ Product findDistinctByName(String name);
+
+ /*Return the products whose price is greater than given price as method parameter*/
+ List<Product> findByPriceGreaterThan(BigDecimal price);
+
+ /*Return the products whose price is less than given price as method parameter*/
+ List<Product> findByPriceLessThan(BigDecimal price);
+
+ /*Return the filtered list of the products that matches the given text*/
+ List<Product> findByNameContaining(String name);
+
+ /*Return products based on SQL like condition*/
+ List<Product> findByNameLike(String name);
+
+ /*Query method to find or retrieve products based on the price range (start price and end price*/
+ List<Product> findByPriceBetween(BigDecimal startPrice, BigDecimal endPrice);
+
+ /*Query method to find or retrieve products based on the created date range (start date and end date*/
+ List<Product> findByDateCratedBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+ //Returns list of products based multiple values
+ List <Product>findByNameIn(List<String> names);
+
+ List<Product> findFirst4ByOrderByNameAsc();
+
+ List<Product>findTop3ByOrderByPriceDesc();
+
+
 
 }
--- spring-data-jpa-course/src/test/java/com/springdatajpa/springboot/repository/QueryMethodsTest.java
+++ spring-data-jpa-course/src/test/java/com/springdatajpa/springboot/repository/QueryMethodsTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -29,5 +33,127 @@ public class QueryMethodsTest {
         System.out.println(product.getDescription());
         System.out.println(product);
     };
+
+    @Test
+    void findByNameOrDescription() {
+        List<Product>products = productRepository.findByNameOrDescription("Yarunon ihmelitku", "Opi Korean perusteet Jane Matalagurun johdolla");
+        for (Product product: products) {
+            System.out.println(product.getId());
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        }
+    }
+
+    @Test
+    void findByNameAndDescription() {
+        List<Product>products = productRepository.findByNameAndDescription("French Cuisine Essentials", "Discover the secrets of French cooking with this comprehensive guide");
+        for (Product product: products) {
+            System.out.println(product.getId());
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        }
+    }
+
+        @Test
+        void findDistinctByName() {
+            Product product = productRepository.findDistinctByName("Essentials of Gardening");
+            System.out.println(product.getId());
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+        }
+
+        @Test
+        void findProductsByPriceLessThan() {
+            BigDecimal price = new BigDecimal(300);
+            List<Product> products = productRepository.findByPriceLessThan(price);
+            for (Product product: products) {
+                System.out.println(product.getId());
+                System.out.println(product.getName());
+                System.out.println(product.getPrice());
+                System.out.println(product.getDescription());
+            }
+        }
+
+        @Test
+        void findProductsByNameContaining() {
+            List<Product> filteredProducts = productRepository.findByNameContaining("Essentials");
+            for (Product product: filteredProducts) {
+                System.out.println(product.getId());
+                System.out.println(product.getName());
+                System.out.println(product.getDescription());
+            }
+        }
+
+    @Test
+    void findProductsByNameLike() {
+        List<Product> filteredProducts = productRepository.findByNameLike("French Cuisine Essentials");
+        for (Product product: filteredProducts) {
+            System.out.println(product.getId());
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+
+        }
+    }
+
+    @Test
+    void findProductsByPriceBetween() {
+        List<Product> filteredProducts = productRepository.findByPriceBetween(new BigDecimal(100), new BigDecimal(350));
+        for (Product product: filteredProducts) {
+            System.out.println(product.getId());
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+            System.out.println(product.getPrice());
+        }
+    }
+
+    @Test
+    void findProductsByDateCreated() {
+        LocalDateTime startTime = LocalDateTime.of(2024, 3, 1, 23, 59);
+        LocalDateTime endTime = LocalDateTime.of(2024, 3, 11, 23, 59);
+
+        List<Product> filteredProducts = productRepository.findByDateCratedBetween(startTime, endTime);
+        for (Product product: filteredProducts) {
+            System.out.println(product.getId());
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+            System.out.println(product.getPrice());
+        }
+    }
+
+    @Test
+    void findProductsByNameIn() {
+
+        List<Product> filteredProducts = productRepository.findByNameIn(List.of("Yarunon ihmelitku", "Essentials of Gardening", "Korean Matalan Gurun Oppikirja"));
+        for (Product product: filteredProducts) {
+            System.out.println(product.getId());
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+            System.out.println(product.getPrice());
+        }
+    }
+
+    @Test
+    void findFirst4ProductsByOrderByNameAsc() {
+        List<Product> products = productRepository.findFirst4ByOrderByNameAsc();
+        for (Product product: products) {
+            System.out.println(product.getId());
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+            System.out.println(product.getPrice());
+        }
+    }
+
+    @Test
+    void findTop3ProductsByPriceDesc() {
+        List<Product> products = productRepository.findTop3ByOrderByPriceDesc();
+        for (Product product: products) {
+            System.out.println(product.getId());
+            System.out.println(product.getName());
+            System.out.println(product.getDescription());
+            System.out.println(product.getPrice());
+        }
+    }
+
+
 
 }
