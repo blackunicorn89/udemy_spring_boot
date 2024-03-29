75397b2dcb60c510305a9b56c22f8dbf166cdb5b
Antti Helander <hela_ant@outlook.com>
2024-03-29 16:41:28 +0200

    Added unversioned files

---
+++ springboot-transaction-demo2/src/main/java/net/javaguides/springboottransactiondemo2/SpringbootTransactionDemo2Application.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboottransactiondemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransactionDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTransactionDemo2Application.class, args);
+	}
+
+}
---
+++ springboot-transaction-demo2/src/test/java/net/javaguides/springboottransactiondemo2/SpringbootTransactionDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboottransactiondemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTransactionDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
