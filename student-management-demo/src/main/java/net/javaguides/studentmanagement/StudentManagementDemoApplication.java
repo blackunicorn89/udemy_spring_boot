db3f924cf7357baa34cf4a762558fb41a1647dbb
Antti Helander <hela_ant@outlook.com>
2024-04-05 12:07:57 +0300

    First commit

---
+++ student-management-demo/src/main/java/net/javaguides/studentmanagement/StudentManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementDemoApplication.class, args);
+	}
+
+}
---
+++ student-management-demo/src/test/java/net/javaguides/studentmanagement/StudentManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
